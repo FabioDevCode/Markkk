e308ac4e15df158f787f49215cc0f4a86e1f657c
FabioDevCode <0677384849@orange.fr>
2026-06-04 22:50:50 +0200

    Revert "Add gotenberg service to convert to PDF"
    This reverts commit e8e2b259e21f94bc6b9914cfa8738377a6434303.

--- vite.config.js
+++ vite.config.js
@@ -48,14 +48,5 @@ export default defineConfig({
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		},
-	},
-	server: {
-		proxy: {
-			'/gotenberg': {
-				target: 'https://demo.gotenberg.dev',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/gotenberg/, ''),
-			},
-		},
-	},
+	}
 })
